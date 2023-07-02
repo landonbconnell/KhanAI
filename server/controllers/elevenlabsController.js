a8df96d3db1077284cb9f9dda589ff76fc43b5bc
FKHAN9644 <frk9644@gmail.com>
2023-07-02 04:57:38 -0400

    Updated voice to be more natural

--- server/controllers/elevenlabsController.js
+++ server/controllers/elevenlabsController.js
@@ -5,10 +5,13 @@ require('dotenv').config({ path: '../.env' });
 const apiKey = process.env.ELEVENLABS_API_KEY;     // Your API key
 const voiceID = process.env.FK_VOICE_ID;           // The ID of the voice you want to get
 const fileName = 'output.mp3';                     // The name of your audio file
+const model_id = "eleven_monolingual_v1";
+
+
 
 const textToSpeech = (textInput) => {
     console.log("textToSpeech function called")
-    voice.textToSpeechStream(apiKey, voiceID, textInput)
+    voice.textToSpeechStream(apiKey, voiceID, textInput,0.4,1.0)
     .then(res => res.pipe(fs.createWriteStream(fileName)))
     .catch(error => console.log(error));
 }
